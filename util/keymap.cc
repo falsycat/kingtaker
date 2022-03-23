c97cc238212722982902a8e041c961cf6f04d1db
falsycat <me@falsy.cat>
2022-03-23 13:23:58 +0900

    add keymap util

---
+++ util/keymap.cc
@@ -0,0 +1,35 @@
+#include "util/keymap.hh"
+
+
+namespace kingtaker {
+
+const std::map<std::string, ImGuiKey> kKeyMap = {
+  { "A", ImGuiKey_A },
+  { "B", ImGuiKey_B },
+  { "C", ImGuiKey_C },
+  { "D", ImGuiKey_D },
+  { "E", ImGuiKey_E },
+  { "F", ImGuiKey_F },
+  { "G", ImGuiKey_G },
+  { "H", ImGuiKey_H },
+  { "I", ImGuiKey_I },
+  { "J", ImGuiKey_J },
+  { "K", ImGuiKey_K },
+  { "L", ImGuiKey_L },
+  { "M", ImGuiKey_M },
+  { "N", ImGuiKey_N },
+  { "O", ImGuiKey_O },
+  { "P", ImGuiKey_P },
+  { "Q", ImGuiKey_Q },
+  { "R", ImGuiKey_R },
+  { "S", ImGuiKey_S },
+  { "T", ImGuiKey_T },
+  { "U", ImGuiKey_U },
+  { "V", ImGuiKey_V },
+  { "W", ImGuiKey_W },
+  { "X", ImGuiKey_X },
+  { "Y", ImGuiKey_Y },
+  { "Z", ImGuiKey_Z },
+};
+
+}  // namespace kingtaker
